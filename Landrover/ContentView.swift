8a2f29fccc494f8870a273df32ce357e2f506457
James <admin@James.local>
2019-12-10 22:06:39 +0300

    Landrover

---
+++ Landrover/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landrover
+//
+//  Created by James on 12/10/19.
+//  Copyright © 2019 James. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
