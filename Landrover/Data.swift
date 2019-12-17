e64f46834163e2a46f1fd802b085ec17c10bfbe5
James <admin@James.local>
2019-12-17 22:47:27 +0300

    setUp data model

--- Landrover/Data.swift
+++ Landrover/Data.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+func load<T:Decodable>(_ filename: String, as type: T.Type = T.self)-> T{
+    let data: Data
+    guard let file = bundle.main.url(forresource: filename, withExtension: nil)
+        else{
+            fatalError("Couldn't find \(filename) in Main bundle")
+    }
+    do{
+        data = try Data(contentsOf: file)
+    }catch{
+        fatalError("Couldn't load \(filename) from main bundle \n: \(error)")
+    }
+    do{
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }catch{
+        fatalError("Couldn't parse \(filename) as \(T.self): \n(error)")
+    }
+}
